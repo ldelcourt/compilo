1eecba2fbdaed51eb29355633fc413c4419e5f3e
PC-Code-HB <pc.code.hb@gmail.com>
2024-04-08 08:53:10 +0200

    Refactor des tests

---
+++ tests/testfiles/a_faire/elseif.c
@@ -0,0 +1,13 @@
+int main() {
+
+  if (4 == 5) {
+    return 0;
+  }
+  else if ( 4+1 == 5) {
+    return 1;
+  }
+  else {
+    return 2;
+  }
+  
+}
---
+++ tests/testfiles/a_faire/var_type_char.c
@@ -0,0 +1,13 @@
+int main () {
+
+  char a;
+  char b;
+
+  a = 4;
+  b = a;
+  
+  char c = a +b;
+
+  return c;
+
+}
